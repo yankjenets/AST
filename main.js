1281476b26057e6a6993458ccae607198a83f5d1
Tyler Healy <tyler_2319@yahoo.com>
2013-01-24 00:54:59 -0500

    Minor magic number fixes

--- main.js
+++ main.js
@@ -103,11 +103,11 @@ function drawSirenBar() {
 
 function drawRoad() {
   ctx.fillStyle = "grey";
-  ctx.fillRect(25, 0, 350, 600)
+  ctx.fillRect(25, 0, canvas.width - 50, canvas.height)
 }
 
 function redrawAll() {
-  ctx.clearRect(0, 0, 400, 400);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   //draw(moose);
   drawRoad();
   roadLines.update();
